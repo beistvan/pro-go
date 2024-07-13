593d881f4e4894f18f530bf18abdba6182c8b557
Beregszászi István <beistvan@gmail.com>
2024-07-13 09:00:10 +0200

    Repeat After Me - Go Without Loop

---
+++ repeat-after-me.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+)
+
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+    _ = scanner.Scan()
+    string1 := scanner.Text()
+    _ = scanner.Scan()
+    string2 := scanner.Text()
+    _ = scanner.Scan()
+    string3 := scanner.Text()
+    fmt.Println(string1)
+    fmt.Println(string2)
+    fmt.Println(string3)
+}
